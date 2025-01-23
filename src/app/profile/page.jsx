db7d33d8fc66e2cd77df86b91afaa8089535c5ca
Raiyan27 <abdullahalraiyan4@gmail.com>
2025-01-23 21:18:42 +0600

    bug fix?

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -5,21 +5,14 @@ export default async function Profile() {
     <div className="flex justify-center items-center min-h-screen bg-gray-100 p-4">
       <div className="flex flex-col md:flex-row justify-center items-center gap-4 bg-white shadow-lg rounded-lg max-w-4xl w-full p-6 space-x-6">
         <img
-          src={user?.picture || "https://placehold.co/400"}
+          src={"https://placehold.co/400"}
           alt="Profile"
           className="w-32 h-32 rounded-full object-cover"
         />
         <div className="flex flex-col space-y-2 text-center">
           <h1 className="text-3xl font-semibold text-gray-800 ">
-            Welcome to your profile, {user?.given_name}!
+            Welcome to your profile
           </h1>
-          <p className="text-lg text-gray-600">Email: {user?.email}</p>
-          {user.username && (
-            <p className="text-lg text-gray-600">Username: {user?.username}</p>
-          )}
-          {user.phone_number && (
-            <p className="text-lg text-gray-600">Phone: {user?.phone_number}</p>
-          )}
         </div>
       </div>
     </div>
