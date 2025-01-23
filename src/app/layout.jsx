d3fd0d73eb5df61b0f72930b43343ee94fcf25da
Raiyan27 <abdullahalraiyan4@gmail.com>
2025-01-23 18:32:14 +0600

    type converted to jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,11 +2,7 @@ import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { AuthProvider } from "./AuthProvider";
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }) {
   return (
     <AuthProvider>
       <html lang="en">
