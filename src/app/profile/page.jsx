a18b1cd4827ca439b4bd9082d0e86c1626a1a2ae
Raiyan27 <abdullahalraiyan4@gmail.com>
2025-01-23 21:12:17 +0600

    possible fix

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,9 +1,12 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+"use client";
+
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import { redirect } from "next/navigation";
 import React from "react";
 
 export default async function Profile() {
-  const user = localStorage.getItem("user");
+  const { user, getUser } = useKindeBrowserClient();
+  const alsoUser = getUser();
 
   if (!user) {
     redirect("/api/auth/login");
@@ -13,20 +16,20 @@ export default async function Profile() {
     <div className="flex justify-center items-center min-h-screen bg-gray-100 p-4">
       <div className="flex flex-col md:flex-row justify-center items-center gap-4 bg-white shadow-lg rounded-lg max-w-4xl w-full p-6 space-x-6">
         <img
-          src={user.picture}
+          src={user?.picture || "https://placehold.co/400"}
           alt="Profile"
           className="w-32 h-32 rounded-full object-cover"
         />
         <div className="flex flex-col space-y-2 text-center">
           <h1 className="text-3xl font-semibold text-gray-800 ">
-            Welcome to your profile, {user.given_name}!
+            Welcome to your profile, {user?.given_name}!
           </h1>
-          <p className="text-lg text-gray-600">Email: {user.email}</p>
+          <p className="text-lg text-gray-600">Email: {user?.email}</p>
           {user.username && (
-            <p className="text-lg text-gray-600">Username: {user.username}</p>
+            <p className="text-lg text-gray-600">Username: {user?.username}</p>
           )}
           {user.phone_number && (
-            <p className="text-lg text-gray-600">Phone: {user.phone_number}</p>
+            <p className="text-lg text-gray-600">Phone: {user?.phone_number}</p>
           )}
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,6 @@ export default async function Navbar() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-  if (user) {
-    localStorage.setItem("user", user);
-  }
-
   return (
     <div className="bg-gray-300">
       <div className="flex items-center justify-between max-w-screen-2xl mx-auto p-4">
