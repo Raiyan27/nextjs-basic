c1734954c766eb9f33dc8e3047764439695c2bae
Raiyan27 <abdullahalraiyan4@gmail.com>
2025-01-23 20:28:47 +0600

    profile route testing

--- src/app/api/auth/[kindeAuth]/route.js
+++ src/app/api/auth/[kindeAuth]/route.js
@@ -1,23 +1,39 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
 
 const handler = handleAuth();
 
-export const GET = async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
+export const GET = async (req) => {
+  console.log("Received request:", req.method);
+
+  const res = NextResponse.next();
+  res.headers.set("Access-Control-Allow-Origin", "*");
+  res.headers.set(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  res.headers.set("Access-Control-Allow-Credentials", "true");
 
   if (req.method === "OPTIONS") {
-    return res.status(200).end();
+    console.log("Handling OPTIONS request");
+    return new NextResponse(null, { status: 200 });
   }
+
   try {
+    console.log("Invoking the authentication handler");
     return handler(req, res);
   } catch (error) {
     console.error("Error in auth handler:", error);
-    return res.status(500).json({ error: "Internal Server Error" });
+    return new NextResponse(
+      JSON.stringify({
+        error: "Internal Server Error",
+        message: error.message,
+      }),
+      { status: 500 }
+    );
   }
 };
